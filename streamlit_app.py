f3694e2e12d7bbd16f8b23f8fca3259953814a25
AlexSnowflake17 <127775490+AlexSnowflake17@users.noreply.github.com>
2023-03-15 10:19:37 -0600

    Update streamlit_app.py
    added fruit_choice text box and connected the output to the request

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,10 @@ streamlit.dataframe(fruits_to_show)
 
 ##New section to display fruityvice response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())  ##Writes the data to the screen
 
 # Takes the json data and puts it into a table/normalizes it(?)
