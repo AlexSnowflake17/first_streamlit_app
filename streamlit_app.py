d0f359431a209e4954d6dbd890ecba6e189d1ffd
AlexSnowflake17 <127775490+AlexSnowflake17@users.noreply.github.com>
2023-03-15 15:53:30 -0600

    Update streamlit_app.py
    added first streamlit stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Takes the normalized data and stores/displays it into a pandas dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+#don't run anything past here
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
