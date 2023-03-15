672832c17d178dced733376e21bf3b705000cdae
AlexSnowflake17 <127775490+AlexSnowflake17@users.noreply.github.com>
2023-03-15 16:08:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#Create function
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    #streamlit.text(fruityvice_response.json())  ##Writes the data to the screen
+
+    # Takes the json data and puts it into a table/normalizes it(?)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Takes the normalized data and stores/displays it into a pandas dataframe
+  return fruityvice_normalized
+
 ##New section to display fruityvice response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -30,13 +40,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #streamlit.text(fruityvice_response.json())  ##Writes the data to the screen
-
-    # Takes the json data and puts it into a table/normalizes it(?)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # Takes the normalized data and stores/displays it into a pandas dataframe
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
